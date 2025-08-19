ce697c9be98f0142a841b3949775a861b4293a6b
Hanna Amaral <hannaap0001@gmail.com>
2025-08-19 20:42:31 -0300

    model

--- Model/Clientes.cs
+++ Model/Clientes.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SistemaAtendimento.Controller
+namespace SistemaAtendimento.Model
 {
     internal class Clientes
     {
