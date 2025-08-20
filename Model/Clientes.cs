6336d6def9665cd10235c0658e4642d1e6ed85f4
Hanna Amaral <hannaap0001@gmail.com>
2025-08-19 21:11:33 -0300

    Desenvolvi a Classe Model Cliente

--- Model/Clientes.cs
+++ Model/Clientes.cs
@@ -8,5 +8,21 @@ namespace SistemaAtendimento.Model
 {
     internal class Clientes
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf_Cnpj { get; set; }
+        public string TipoPessoa { get; set; }
+        public string Telefone { get; set; }
+        public string Celular { get; set; }
+        public string Cep {  get; set; }
+        public string endereco { get; set; }
+        public string numero { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string cidade { get; set; }
+        public string estado { get; set; }
+        public int ativo { get; set; }
+
     }
 }
